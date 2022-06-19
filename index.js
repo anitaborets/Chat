e315ffb17e205c6e94b019ee4dab056ec498ef40
Anna Borets <85034157+anitaborets@users.noreply.github.com>
2022-06-19 12:44:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io');
+const {Server} = require("socket.io");
+io = new Server(server);
+server.listen(3000);
+
+app.get('/', function (request, respons) {
+    respons.sendFile(__dirname + '/index.html');
+});
+
+connections = [];
+
+io.sockets.on('connection', function (socket) {
+    console.log("Successful connection");
+    connections.push(socket);
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnect");
+    });
+    socket.on('send mess', function (data) {
+        io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+    });
+});
